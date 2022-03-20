e42d3e363e5bf0e2af2368fcb316c219190065f0
JonathanAndrade19 <jonathanjampamb@gmail.com>
2022-03-20 08:10:38 -0300

    tuning the list-render component and creating an interface

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Animal } from 'src/app/interface/Animal';
 
 @Component({
   selector: 'app-list-render',
@@ -6,16 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-render.component.scss']
 })
 export class ListRenderComponent implements OnInit {
-  animals = [
-    { name: 'Nick', type: 'Cat'},
-    { name: 'Nina', type: 'Cat'},
-    { name: 'Marley', type: 'Dog'},
-    { name: 'Lalá', type: 'Dog'},
-    { name: 'Lyncou', type: 'Dog'},
+  animals: Animal[] = [
+    { name: 'Nick', type: 'Cat', age: 1},
+    { name: 'Nina', type: 'Cat', age: 2},
+    { name: 'Marley', type: 'Dog', age: 7},
+    { name: 'Lalá', type: 'Dog', age: 6},
+    { name: 'Lyncou', type: 'Dog', age: 5},
   ]
+
+  animalDetails = '';
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  showAge(animal: Animal): void {
+    this.animalDetails = `O pet ${animal.name} tem ${animal.age} Anos!`;
+    console.log(this.animalDetails);
   }
 
 }
