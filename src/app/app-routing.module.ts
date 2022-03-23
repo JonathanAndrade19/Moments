b0875fbdaf7a43e1e42f5c3bc3a91f21d0536104
JonathanAndrade19 <jonathanjampamb@gmail.com>
2022-03-22 22:44:19 -0300

    app-routing module implements

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirsComponentComponent } from './components/firs-component/firs-component.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
+import { TwoWayBindingComponent } from './components/two-way-binding/two-way-binding.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: FirsComponentComponent},
+  {path:'list', component: ListRenderComponent},
+  {path:'two-way-binding', component: TwoWayBindingComponent}
+];
 
 @NgModule({
+  declarations: [],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
